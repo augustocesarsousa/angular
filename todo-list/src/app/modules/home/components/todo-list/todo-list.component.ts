1001b921161a51c1422396e60f1edfd28c51fe29
Augusto Cesar <acs-03@hotmail.com>
2023-05-01 21:37:44 -0300

    TO-DO List - Adicionado localStorage para salvar a lista de tarefas

--- todo-list/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ todo-list/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -7,12 +7,12 @@ import { TaskList } from '../../model/task-list';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements DoCheck {
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = JSON.parse(
+    localStorage.getItem('list') || '[]'
+  );
 
   ngDoCheck(): void {
-    this.taskList.sort(
-      (fist, last) => Number(fist.checked) - Number(last.checked)
-    );
+    this.setLocalStorage();
   }
 
   public setEmitTaskList(event: string) {
@@ -36,6 +36,15 @@ export class TodoListComponent implements DoCheck {
       if (confirm) {
         this.deleteItemTaskList(index);
       }
+    }
+  }
+
+  public setLocalStorage() {
+    if (this.taskList) {
+      this.taskList.sort(
+        (fist, last) => Number(fist.checked) - Number(last.checked)
+      );
+      localStorage.setItem('list', JSON.stringify(this.taskList));
     }
   }
 }
