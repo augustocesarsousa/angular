576ffbd4eee0fc529ada9f6661cec5cdf049e791
Augusto Cesar <acs-03@hotmail.com>
2023-05-11 18:06:58 -0300

    Pokedex - Criado modulo shared e componentes poke-header, poke-search e poke-list

--- pokedex/src/app/pages/pages.module.ts
+++ pokedex/src/app/pages/pages.module.ts
@@ -5,9 +5,10 @@ import { RoutingModule } from './routing.module';
 
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent, DetailsComponent],
-  imports: [CommonModule, RoutingModule],
+  imports: [CommonModule, RoutingModule, SharedModule],
 })
 export class PagesModule {}
