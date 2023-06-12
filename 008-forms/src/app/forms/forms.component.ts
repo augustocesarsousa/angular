bacc94a7cc8aa306ed83b501c510194e37c53d9e
Augusto Cesar <acs-03@hotmail.com>
2023-06-12 18:43:16 -0300

    Forms - Submit

--- 008-forms/src/app/forms/forms.component.ts
+++ 008-forms/src/app/forms/forms.component.ts
@@ -1,10 +1,14 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent {
-
+  public submitForm(form: NgForm): void {
+    alert(JSON.stringify(form.value));
+  }
 }
