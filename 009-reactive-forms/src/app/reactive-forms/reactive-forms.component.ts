4a8adacfa16503324dc363119ff6e0b1657e8b40
Augusto Cesar <acs-03@hotmail.com>
2023-06-13 13:40:13 -0300

    Reactive Forms - FormGroup

---
+++ 009-reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.scss'],
+})
+export class ReactiveFormsComponent {
+  public cadastroForm: FormGroup = this.formBuilder.group({
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  public submitForm(): void {
+    console.log(this.cadastroForm.value);
+  }
+}
