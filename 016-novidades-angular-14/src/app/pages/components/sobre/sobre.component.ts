ab294d69b2c7f1f18ec2dd51c0b73675c270d126
Augusto Cesar <acs-03@hotmail.com>
2023-06-20 22:36:33 -0300

    Novidades Angular 14 - ReactiveForms tipados

--- 016-novidades-angular-14/src/app/pages/components/sobre/sobre.component.ts
+++ 016-novidades-angular-14/src/app/pages/components/sobre/sobre.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sobre.component.html',
-  styleUrls: ['./sobre.component.scss']
+  styleUrls: ['./sobre.component.scss'],
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit {
+  public form = this.formBuilder.group({
+    email: ['acs-03@hotmail.com'],
+    password: [''],
+  });
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log(this.form.value.email);
+  }
 }
